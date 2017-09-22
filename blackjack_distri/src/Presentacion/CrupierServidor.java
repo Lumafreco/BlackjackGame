a2c576a4d89d98c90698fcd93ddfb8f69cf212e7
Ricardo Bernal <lumafreco@users.noreply.github.com>
2017-09-22 15:47:09 -0500

    G

--- blackjack_distri/src/Presentacion/CrupierServidor.java
+++ blackjack_distri/src/Presentacion/CrupierServidor.java
@@ -54,6 +54,10 @@ public class CrupierServidor extends Thread{
 	}//Fin CrupierServidor()
 	
 	
+	/**
+	 * Metodo por defecto para Thread
+	 */
+	
 	public void run(){
 		System.out.println("Servidor en ejeccucion.......");
 		
@@ -91,33 +95,38 @@ public class CrupierServidor extends Thread{
 		}//Fin While(true)
 		
 	}//Fin Thread Run()
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		
-		ServerSocket socket;
-		boolean finnal = false;
-		
-		try{
-			socket = new ServerSocket(6000);
-			Socket socket_cli = socket.accept();
-			
-			
-			DataInputStream in =
-					new DataInputStream(socket_cli.getInputStream());
-			
-			do{
-				String mensaje ="";
-				mensaje = in.readUTF();
-				System.out.println(mensaje);
-				} while(1>0);
-			}
-		catch (Exception e){
-			
-			System.err.println(e.getMessage());
-			System.exit(1);
-		}
-	}	
-
+	
 }
+
+
+
+
+/*
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	
+	
+	ServerSocket socket;
+	boolean finnal = false;
+	
+	try{
+		socket = new ServerSocket(6000);
+		Socket socket_cli = socket.accept();
+		
+		
+		DataInputStream in =
+				new DataInputStream(socket_cli.getInputStream());
+		
+		do{
+			String mensaje ="";
+			mensaje = in.readUTF();
+			System.out.println(mensaje);
+			} while(1>0);
+		}
+	catch (Exception e){
+		
+		System.err.println(e.getMessage());
+		System.exit(1);
+	}
+}
+*/
